13b27b451bbe0e40f9095cadb785cd5c50a1ec30
Andy Huang <hpsh31323@gmail.com>
2020-06-10 17:47:01 +0800

    new view manager_index.html

--- py_project/settings.py
+++ py_project/settings.py
@@ -125,4 +125,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 """
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- py_start/views.py
+++ py_start/views.py
@@ -9,12 +9,14 @@ def project_index(request):
 
 
 def pos_system_index(request):
-    return render(request, "pos_system_index.html", locals())
+    return render(request, "pos/pos_system_index.html", locals())
 
 
-def inner_index(request):
-    return render(request, "inner_index.html", locals())
+def manager_index(request):
+    return render(request, "cms/manager_index.html", locals())
 
 
 def inner_login_page(request):
-    return render(request, "inner_login_page.html", locals())
+    return render(request, "cms/inner_login_page.html", locals())
+
+
