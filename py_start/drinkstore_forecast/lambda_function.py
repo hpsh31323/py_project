65f6dad1c25ff04670cfd0413858f9384d088dac
hpsh31323 <hpsh31323@gmail.comn>
2020-07-05 07:27:06 +0800

    add print

--- py_start/drinkstore_forecast/lambda_function.py
+++ py_start/drinkstore_forecast/lambda_function.py
@@ -12,14 +12,14 @@ def forecasting():
     start = time()
     dict1 = task1_get_past_TempAndOrder(180)
     point1 = time()
-    task1 = "task1 cost %f sec" % (point1 - start)
+    print("task1 success cost %f sec" % (point1 - start))
     point2 = time()
     dict2 = task2_forecasting_Order(dict1["list_max_temps"], dict1["list_min_temps"], dict1["list_orders"],
                                     dict1["dict_future_temp"])
     point3 = time()
-    task2 = "task2 cost %f sec" % (point3 - point2)
+    print("task2 success cost %f sec" % (point3 - point2))
     exportImgF(dict2)
     end = time()
-    total_time = "total cost %f sec" % (end - start)
+    print("total success cost %f sec" % (end - start))
 
     # return {"task1": task1, "task2": task2, "total_time": total_time}
